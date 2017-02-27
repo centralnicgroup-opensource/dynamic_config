a50b6c544a852c727b07431fef2a6734f32e6156
Lenz Gschwendtner <lenz@springtimesoft.com>
2017-02-27 01:17:49 +0100

    updater works and reports errors if things go wrong

--- lib/dynamic_config/updater.ex
+++ lib/dynamic_config/updater.ex
@@ -1,21 +1,56 @@
 defmodule DynamicConfig.Updater do
 
   require Logger
+  use GenServer
+
   alias DynamicConfig.CouchDB, as: Source
-  
-  def update do
+
+
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def update, do: GenServer.info(__MODULE__, :update)
+
+  def init(_args) do
+    Process.send_after(__MODULE__, {:update}, 1000)
+    {:ok, %{}}
+  end
+
+  # callbacks
+
+  def handle_info({:update}, state) do
+    state1 = case update_config() do
+      :ok ->
+        Map.put(state, :last_updated, DateTime.utc_now)
+      {:error, error} ->
+        s2 = Map.put(state, :last_updated, DateTime.utc_now)
+        Map.put(s2, :error, error)
+    end
+    interval = Application.get_env(__MODULE__, :interval, 50_000)
+    Process.send_after(__MODULE__, {:update}, interval)
+    {:noreply, state1}
+  end
+
+
+  defp update_config do
     key = Application.get_env(__MODULE__, :update, Application.get_application(__MODULE__))
     case Source.get_config(key) do
       {:ok, config} ->
         case config["_rev"] == Application.get_env(key, "_rev") do
           true -> 
             Logger.debug("no need to update, already have the latest version: #{config["_rev"]}")
+            :ok
           false ->
             Logger.debug("need to update, have a new version: #{config["_rev"]}")
             config
             |> Enum.each(fn({k, v}) -> Application.put_env(key, k, v, [:persistent]) end)
+            :ok
         end 
-      error -> error
+      error -> 
+        Logger.error("Had errors updating the config for #{key}: #{inspect error}")
+        {:error, error}
     end
   end
 end
